5f2d8710b72e4746a52b7686b3dbaf938d0345ee
Oleksandr Tokar <oleksandr.tokar.publish@gmail.com>
2023-12-04 19:30:20 +0300

    Create some logic of attaching to needed config

--- Assets/Packages/ThroneAndLibertySpellBuilder/Scripts/Data/WeaponTypeData.cs
+++ Assets/Packages/ThroneAndLibertySpellBuilder/Scripts/Data/WeaponTypeData.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Packages.ThroneAndLibertySpellBuilder.Scripts.Data.Enums;
 using UnityEngine;
 
@@ -11,5 +12,42 @@ namespace Packages.ThroneAndLibertySpellBuilder.Scripts.Data
         
         public List<SpellData> ActiveSpells;
         public List<SpellData> PassiveSpells;
+
+        public void AddSpell(SpellData spellData)
+        {
+            switch (spellData.Type)
+            {
+                case SpellType.Active:
+                    AddActiveSpell(spellData);
+                    break;
+                case SpellType.Passive:
+                    AddPassiveSpell(spellData);
+                    break;
+                case SpellType.Undefined:
+                    throw new Exception("There are no such spell type");
+            }
+        }
+
+        private void AddActiveSpell(SpellData spellData)
+        {
+            if (ActiveSpells.Contains(spellData))
+            {
+                Debug.LogError("You already have that spell, so spells should be unique");
+                return;
+            }
+
+            ActiveSpells.Add(spellData);
+        }
+
+        private void AddPassiveSpell(SpellData spellData)
+        {
+            if (PassiveSpells.Contains(spellData))
+            {
+                Debug.LogError("You already have that spell, so spells should be unique");
+                return;
+            }
+            
+            PassiveSpells.Add(spellData);
+        }
     }
 }
--- Assets/Packages/ThroneAndLibertySpellBuilder/Scripts/SpellGenerator/SpellGenerator.cs
+++ Assets/Packages/ThroneAndLibertySpellBuilder/Scripts/SpellGenerator/SpellGenerator.cs
@@ -2,6 +2,7 @@
 using Packages.ThroneAndLibertySpellBuilder.Scripts.Data;
 using Packages.ThroneAndLibertySpellBuilder.Scripts.Data.Enums;
 using Sirenix.OdinInspector;
+using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -36,7 +37,40 @@ namespace Packages.ThroneAndLibertySpellBuilder.Scripts.SpellGenerator
         [Button("Generate"), ShowIf("CanGenerate")]
         public void Generate()
         {
+            var createdSpell = CreateSpellDataAndMoveToFolder();
+            GeneralDataBase.WeaponTypeToDataInstanceMap[WeaponVariant].AddSpell(createdSpell);
             
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+        
+        
+        private SpellData CreateSpellDataAndMoveToFolder()
+        {
+            // Create an instance of the ScriptableObject
+            SpellData newInstance = ScriptableObject.CreateInstance<SpellData>();
+
+            newInstance.Name = Name;
+            newInstance.Description = Description;
+            newInstance.Effect = Effect;
+            newInstance.Cooldown = Cooldown;
+            newInstance.Texture = Texture;
+            newInstance.Type = Type;
+            
+            string folderPath = $"Assets/Packages/ThroneAndLibertySpellBuilder/Data/Skills/{WeaponVariant}";
+
+            // Ensure the folder exists, create it if it doesn't
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                AssetDatabase.CreateFolder("Assets/Packages/ThroneAndLibertySpellBuilder/Data/Skills", $"{WeaponVariant}");
+            }
+
+            string assetPath = folderPath + "/" + $"{Name}.asset";
+
+            // Create the asset at the specified path
+            AssetDatabase.CreateAsset(newInstance, assetPath);
+
+            return newInstance;
         }
 
         private bool IsNotEmpty(string value) => value != String.Empty;
